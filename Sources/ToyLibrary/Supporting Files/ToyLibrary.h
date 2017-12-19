317c3a7358fd629e0b3368503f7f186b384b386c
Faiz Mokhtar <mfmokhtar@gmail.com>
2017-12-19 13:28:40 +0800

    Use a single plist for all platforms.

---
+++ Sources/ToyLibrary/Supporting Files/ToyLibrary.h
@@ -0,0 +1,18 @@
+//
+//  ToyLibrary.h
+//  ToyLibrary
+//
+//  Created by Faiz Mokhtar on 19/12/2017.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ToyLibrary.
+FOUNDATION_EXPORT double ToyLibraryVersionNumber;
+
+//! Project version string for ToyLibrary.
+FOUNDATION_EXPORT const unsigned char ToyLibraryVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ToyLibrary/PublicHeader.h>
+
+
