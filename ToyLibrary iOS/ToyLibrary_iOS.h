892374654b86b9c418482d8454a20233a116592e
Faiz Mokhtar <mfmokhtar@gmail.com>
2017-12-19 13:12:54 +0800

    Initial commit

---
+++ ToyLibrary iOS/ToyLibrary_iOS.h
@@ -0,0 +1,18 @@
+//
+//  ToyLibrary_iOS.h
+//  ToyLibrary iOS
+//
+//  Created by Faiz Mokhtar on 19/12/2017.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ToyLibrary_iOS.
+FOUNDATION_EXPORT double ToyLibrary_iOSVersionNumber;
+
+//! Project version string for ToyLibrary_iOS.
+FOUNDATION_EXPORT const unsigned char ToyLibrary_iOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ToyLibrary_iOS/PublicHeader.h>
+
+
---
+++ ToyLibrary watchOS/ToyLibrary_watchOS.h
@@ -0,0 +1,18 @@
+//
+//  ToyLibrary_watchOS.h
+//  ToyLibrary watchOS
+//
+//  Created by Faiz Mokhtar on 19/12/2017.
+//
+
+#import <WatchKit/WatchKit.h>
+
+//! Project version number for ToyLibrary_watchOS.
+FOUNDATION_EXPORT double ToyLibrary_watchOSVersionNumber;
+
+//! Project version string for ToyLibrary_watchOS.
+FOUNDATION_EXPORT const unsigned char ToyLibrary_watchOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ToyLibrary_watchOS/PublicHeader.h>
+
+
